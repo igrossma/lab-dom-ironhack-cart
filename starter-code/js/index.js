81e632281d2eb1b333caf71aaa1450586ccbc88e
Ingo Grossmann <ingogrossmann@icloud.com>
2019-06-11 09:29:32 +0100

    general set up

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,22 @@
+// to select DOM Elements
+
+let $productName = document.getElementsByClassName("product-name")
+let $productQuantity = document.querySelector("input")
+let $productPrice = document.getElementsByClassName("product-price")
+let $totalPriceItem = document.getElementsByClassName("total-price-item")
+let $button = document.querySelector("button")
+
+// to get Quantity of Product through event
+
+$button.onclick = function() {calculateItems()}
+
+function calculateItems() 
+{ 
+  let value = $productQuantity.value
+  console.log ("The button was clicked and the value is", value)
+}
+
+  
 function deleteItem(e){
 
 }
@@ -15,6 +34,7 @@ function getTotalPrice() {
 }
 
 function createQuantityInput(){
+
 
 }
 
@@ -44,6 +64,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
+
   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
